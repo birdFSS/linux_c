2451b9fa10753cb2b6b8e23d6393d7d5bde8dd80
birdFSS <ffh465031824@163.com>
2019-09-28 09:01:52 +0800

    show_bytes

--- myTest/mmap_test.c
+++ myTest/mmap_test.c
@@ -4,7 +4,7 @@ int main(int argc, char* argv[])
 {
     int fd = open("file", O_RDONLY| O_CREAT, 0666);
     ERROR_CHECK(fd, -1, "open");
-    char* p=(char*)mmap(NULL, 4096, PROT_WRITE, MAP_SHARED, fd, 0);
+    char* p=(char*)mmap(NULL, 1024 * 1024 * 1024, PROT_WRITE, MAP_SHARED, fd, 0);
     ERROR_CHECK(p, (char*)-1, "mmap");
     int ret = munmap(p, 4096);
     ERROR_CHECK(ret, -1, "mummap");
