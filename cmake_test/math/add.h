969957db7988011da7001b72d8b129a04714e202
Fenghai Fu <ffhbird@gmail.com>
2021-03-14 15:58:52 +0800

    add cmake test and boost

---
+++ cmake_test/math/add.h
@@ -0,0 +1,57 @@
+/* 
+ * file : fileName.h
+ * brief : 
+ * author : user
+ * 
+ * change log : date create this file
+ * 
+ * 
+ */ 
+
+#ifndef __ADD_H__
+#define __ADD_H__
+
+/*************************** INCLUDE *********************************************/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+/*************************** DEFINE **********************************************/
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+/*************************** STRUCT ****************************************/
+
+/*************************** TYPEDEF *********************************************/
+
+/*************************** GLOBAL FUNCTION *************************************/
+/* fn 
+ * brief 
+ * param[in] 
+ * param[out] 
+ * param[in] 
+ * param[in] 
+ */
+int add(int a, int b);
+
+
+
+
+
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ cmake_test/src/test.c
@@ -0,0 +1,9 @@
+#include "add.h"
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello world, %d = %d + %d\n", add(3, 4), 3, 4);
+    return 0;
+}
+
